693a638270cdf1c4d565a916da9f47a3f7f9bb3d
Dx Dev <137692796+yusufcsknn@users.noreply.github.com>
2025-09-19 22:34:26 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
 Config = {}
 
 -- Discord log webhook
-Config.Webhook = "https://discord.com/api/webhooks/1418627600389246986/ICgnDXBWi6nteYPic2xeKaY4Qhl3qLOY7MBBstOo-0KzqjU9kHX5bWnw9rLJ933OaRpR"
+Config.Webhook = "_DİSCORD_WEEBHOOK_LİNKİ_"
